e0522aea7061ecece754d99f6179c624557e3703
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-03-18 16:41:02 -0300

    code reviewd

--- codigo.py
+++ codigo.py
@@ -1,89 +1,17 @@
-# Hashzap
-# botao de iniciar chat
-# popup para entrar no chat
-# quando entrar no chat: (aparece para todo mundo)
-    # a mensagem que você entrou no chat
-    # o campo e o botão de enviar mensagem
-# a cada mensagem que você envia (aparece para todo mundo)
-    # Nome: Texto da Mensagem
+# Function for nth Fibonacci number
 
+def Fibonacci(n):
+	if n<= 0:
+		print("Incorrect input")
+	# First Fibonacci number is 0
+	elif n == 1:
+		return 0
+	# Second Fibonacci number is 1
+	elif n == 2:
+		return 1
+	else:
+		return Fibonacci(n-1)+Fibonacci(n-2)
 
-# produto = {
-#     "nome": "iphone",
-#     "preço": 6500,
-#     "quantidade": 150    
-# }
+# Driver Program
 
-# produto["quantidade"]
-
-import flet as ft
-
-def main(pagina):
-    texto = ft.Text("Hashzap")
-
-    chat = ft.Column()
-
-    nome_usuario = ft.TextField(label="Escreva seu nome")
-
-    def enviar_mensagem_tunel(mensagem):
-        tipo = mensagem["tipo"]
-        if tipo == "mensagem":
-            texto_mensagem = mensagem["texto"]
-            usuario_mensagem = mensagem["usuario"]
-            # adicionar a mensagem no chat
-            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
-        else:
-            usuario_mensagem = mensagem["usuario"]
-            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", 
-                                         size=12, italic=True, color=ft.colors.ORANGE_500))
-        pagina.update()
-
-    pagina.pubsub.subscribe(enviar_mensagem_tunel)
-
-    def enviar_mensagem(evento):
-        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value,
-                                "tipo": "mensagem"})
-        # limpar o campo de mensagem
-        campo_mensagem.value = ""
-        pagina.update()
-
-    campo_mensagem = ft.TextField(label="Digite uma mensagem", on_submit=enviar_mensagem)
-    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
-
-    def entrar_popup(evento):
-        pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
-        # adicionar o chat
-        pagina.add(chat)
-        # fechar o popup
-        popup.open = False
-        # remover o botao iniciar chat
-        pagina.remove(botao_iniciar)
-        pagina.remove(texto)
-        # criar o campo de mensagem do usuario
-        # criar o botao de enviar mensagem do usuario
-        pagina.add(ft.Row(
-            [campo_mensagem, botao_enviar_mensagem]
-        ))
-        pagina.update()
-
-    popup = ft.AlertDialog(
-        open=False, 
-        modal=True,
-        title=ft.Text("Bem vindo ao Hashzap"),
-        content=nome_usuario,
-        actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)],
-        )
-
-    def entrar_chat(evento):
-        pagina.dialog = popup
-        popup.open = True
-        pagina.update()
-
-    botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
-
-    pagina.add(texto)
-    pagina.add(botao_iniciar)
-
-ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
-
-# deploy
+print(Fibonacci(10))
