288ff7a18fea69f702638f04828e27b1646fcdba
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-04-23 15:22:05 -0300

    new math code

--- codigo.py
+++ codigo.py
@@ -5,7 +5,7 @@ def funcao_principal():
     linha2 = formulario.lineEdit_2.text()
     linha3 = formulario.lineEdit_3.text()
 
-    
+   
     if formulario.radioButton.isChecked() :
         print("Categoria Eletronicos selecionada")
     elif formulario.radioButton_2.isChecked() :
@@ -25,3 +25,12 @@ formulario.pushButton.clicked.connect(funcao_principal)
 
 formulario.show()
 app.exec()
+
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
