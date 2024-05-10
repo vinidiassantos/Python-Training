db92b7ce84bafb076d2687d4093a5a311dfac4b7
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-10 14:26:04 -0300

    Update newcode.py

--- newcode.py
+++ newcode.py
@@ -1,7 +1,16 @@
-nome =="leon"
-if nome =="Leon":
-    print("Acertou meu nome")
-if __name__
-if continue
-staticmethod
+# Python3 program to swap first
+# and last element of a list
 
+#Swap function
+def swapList(newList):
+    size = len(newList)
+     
+    # Swapping 
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+     
+    return newList
+     
+# Driver code
+newList = [12, 35, 9, 56, 24]
