5ec1b82c717202b8c2b3c5fa5768800abc71dab8
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-12 19:55:19 -0300

    new code

---
+++ conexao.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+def criar_conexao(host, usuario, senha, banco):
+        return mysql.connector.connect(host=host, user=usuario, password=senha, database=banco)
+
+def fechar_conexao(con):
+        return con.close()
+
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from conexao import criar_conexao. fechar_conexao
+
+def main():
+
+    if __name__ == "__main__":
+        
