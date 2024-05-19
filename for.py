c3cd03b3525e44acde284ee13c2ccee109fd8552
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-19 19:15:16 -0300

    Update for.py

--- for.py
+++ for.py
@@ -1,4 +1,7 @@
-lista = [2,6,9,4,0,12,3,7]
-palavra = 'vini'
-for letra in palavra:
-    print(letra)
+# lista = [2,6,9,4,0,12,3,7]
+# palavra = 'vini'
+# for letra in palavra:
+#     print(letra)
+
+for numero in range(1,11):
+    print(numero)
