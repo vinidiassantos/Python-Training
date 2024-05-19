d5a1002654a2412c4b2f9cb98109e0ef97438b2e
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-19 16:13:50 -0300

    Update print.py

--- print.py
+++ print.py
@@ -1,8 +1,8 @@
 # sintaxe:
 # print(objetos, argumentos)
 
-nome = 'maria'
-idade = 30
-msg_formatada = 'o nome dela é {0} e ela tem {1} anos'.format(nome,idade)
-print(msg_formatada)
+nome = 'vini'
+peso = 73
+msg = f'olá, meu nome é {nome} e eu {peso} quilos.'
+print(msg)
 
