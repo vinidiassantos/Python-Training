11838354a44f4a21de76cac71d976646d9ad1317
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-19 19:22:56 -0300

    code

--- for.py
+++ for.py
@@ -3,5 +3,10 @@
 # for letra in palavra:
 #     print(letra)
 
-for numero in range(1,11):
-    print(numero)
+nome = input('digite seu nome: ')
+for x in range(10):
+    print(f'{x+1} {nome}');
+
+# range(valor_inicial, valor_final, incremento)
+for x in range(2,20,-2):
+    print(x)
