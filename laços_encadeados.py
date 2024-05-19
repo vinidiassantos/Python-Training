998a418de5e1ea0cfa7244627182db320bf12569
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-05-19 19:30:52 -0300

    Update laços_encadeados.py

---
+++ laços_encadeados.py
@@ -0,0 +1,13 @@
+# for cont_ex in range(1,6):
+#     print(f'\nRodada: {cont_ex}')
+#     for cont_in in range(5, 0, -1):
+#         print(f'Valor: {cont_in}')
+
+# print('fim dos laços')
+
+import random
+for A in range(1,6):
+    print(f'\Conjunto {A}')
+    for B in range(5):
+        num = random.randint(1,100)
+        print(f'Valor: {num}')
