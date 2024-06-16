2459b2ae505330650e59ef62323fb4ceb6e3dc1c
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-06-16 20:26:43 -0300

    Update matemathc.py

--- matemathc.py
+++ matemathc.py
@@ -1,12 +1,7 @@
-# Funções builtin
+import math
 
-valores = [2,5,8,-1,0,11,7,-6]
-print(max(valores))
-print(min(valores))
-a = -5
-b = 4
-print(abs(a))
-print(pow(a, b))
-c = 2.789011
-print(round(c,1))
+x = 8
+y = 100
 
+raiz_quadrada = math.sqrt(x)
+print(round(raiz_quadrada, 2))
