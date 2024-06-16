9db517bcae30ba80f454134f2e4771bef5eafb4f
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-06-16 20:27:43 -0300

    Update matemathc.py

--- matemathc.py
+++ matemathc.py
@@ -3,5 +3,8 @@ import math
 x = 8
 y = 100
 
-raiz_quadrada = math.sqrt(x)
-print(round(raiz_quadrada, 2))
+# raiz_quadrada = math.sqrt(x)
+# print(round(raiz_quadrada, 2))
+
+logaritmo = math.log10(y)
+print(logaritmo)
