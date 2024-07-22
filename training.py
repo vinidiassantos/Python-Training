1f765058f5d39a343a7347359f604306112b80de
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-07-22 19:54:51 -0300

    Update training.py

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 #Este é um comentário#
-nota = 5
-if nota >=7:
-    print("Aprovado")
-else:
-    print("Reprovado")
+#nota = 5
+#if nota >=7:
+#    print("Aprovado")
+#else:
+#    print("Reprovado")
     
