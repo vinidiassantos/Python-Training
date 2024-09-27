5f6aab05b176cf9a32c835185ce1b39ec0fd9f48
Vini DiaSSantos <vinidiassantos@gmail.com>
2024-09-27 08:38:19 -0300

    Update machineone.py

--- machineone.py
+++ machineone.py
@@ -4,10 +4,12 @@ from delta.tables import *
 # Carregar tabelas Delta como DeltaTable
 deltaTable_orders = DeltaTable.forPath(spark, "dbfs:/FileStore/tables/datalake/delta/orders.delta")
 deltaTable_order_details = DeltaTable.forPath(spark, "dbfs:/FileStore/tables/datalake/delta/orderdetails.delta")
+deltaTable_orders = DeltaTable.forPath(spark, "dbfs:/FileStore/tables/datalake/delta/orders.delta")
+
 
 # Criar os novos registros que queremos inserir
 new_order = spark.createDataFrame([(11078, "ALFKI", 1, "2023-08-01")], ["OrderID", "CustomerID", "EmployeeID", "OrderDate"])
-new_order_details = spark.createDataFrame([(11078, 2, 18)], ["OrderID", "ProductID", "UnitPrice", "Quantity"])
+new_order_details = spark.createDataFrame([(11078, 1, 2, 18, 32)], ["OrderID", "ProductID", "UnitPrice", "Quantity"])
 
 deltaTable_orders.alias("orders").merge(
     new_order.alias("newOrder"),
